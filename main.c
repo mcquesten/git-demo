b149380fa626662ace58a74a9bffd3f465dd39de
mcquesten <rmcquest@asu.edu>
2026-03-12 13:20:20 -0700

    add program

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char str[100];
+	printf("Enter a string: ");
+	fgets(str, sizeof(str), stdin);
+
+	// Remove newline character if present
+	str[strcspn(str, "\n")] = '\0';
+
+	printf("You entered: %s\n", str);
+
+	return 0;
+}
