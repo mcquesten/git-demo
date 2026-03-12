ccdd1059201ed31c5592b3b6cc098560793a1073
mcquesten <rmcquest@asu.edu>
2026-03-12 13:23:35 -0700

    update feature

--- main.c
+++ main.c
@@ -3,14 +3,6 @@
 #include <string.h>
 
 int main() {
-	char str[100];
-	printf("Enter a string: ");
-	fgets(str, sizeof(str), stdin);
-
-	// Remove newline character if present
-	str[strcspn(str, "\n")] = '\0';
-
-	printf("You entered: %s\n", str);
-
+	printf("Hello, World\n");
 	return 0;
 }
